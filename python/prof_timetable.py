3a55d069e44c718932899a776f0e7ad99875a577
SIKU-KR <79081225+SIKU-KR@users.noreply.github.com>
2023-11-20 16:00:58 +0900

    Update prof_timetable.py

--- python/prof_timetable.py
+++ python/prof_timetable.py
@@ -8,7 +8,7 @@ def getProflist():
     try:
         # df = pd.read_csv(open_url("https://raw.githubusercontent.com/SIKU-KR/SIKU-KR.github.io/main/pages/process_final.csv"))
         # df = pd.read_csv('process_final.csv')
-        df = pd.read_csv('https://raw.githubusercontent.com/SIKU-KR/SIKU-KR.github.io/main/python/process_final.csv?raw=true')
+        df = pd.read_csv('https://repo.codereview.online/ppark/project-x/-/raw/main/process_final.csv')
         df.dropna(inplace=True)
         room_set = set(df['교수'])
         room_list = list(room_set)
