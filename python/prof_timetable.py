f66371646cc3555c4f2f0732da661d1da8d01c4c
SIKU-KR <79081225+SIKU-KR@users.noreply.github.com>
2023-11-20 15:21:06 +0900

    Update prof_timetable.py

--- python/prof_timetable.py
+++ python/prof_timetable.py
@@ -6,8 +6,8 @@ from pyodide.http import open_url
 
 def getProflist():
     try:
-        # df = pd.read_csv(open_url("http://127.0.0.1:5500/pages/process_final.csv"))
-        df = pd.read_csv('../pages/process_final.csv')
+        df = pd.read_csv(open_url("https://raw.githubusercontent.com/SIKU-KR/SIKU-KR.github.io/main/pages/process_final.csv"))
+        # df = pd.read_csv('../pages/process_final.csv')
         df.dropna(inplace=True)
         room_set = set(df['교수'])
         room_list = list(room_set)
@@ -20,8 +20,8 @@ def make_timetable(prof):
                         '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30']
 
     try:
-        # df = pd.read_csv(open_url("http://127.0.0.1:5500/pages/process_final.csv"))
-        df = pd.read_csv('../pages/process_final.csv')
+        df = pd.read_csv(open_url("https://raw.githubusercontent.com/SIKU-KR/SIKU-KR.github.io/main/pages/process_final.csv"))
+        # df = pd.read_csv('../pages/process_final.csv')
         df.dropna(inplace=True)
     except FileNotFoundError:
         print('전처리 된 파일이 없습니다.')
